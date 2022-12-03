4b74626897efbd961e61e24f9b0053296df06382
richod1 <frimpsup@gmail.com>
2022-12-03 05:22:12 -0800

    add:postCard

--- frontend/src/components/PostCards.js
+++ frontend/src/components/PostCards.js
@@ -1,9 +1,104 @@
-import React from 'react'
+import React,{useState} from 'react'
+import styled from 'styled-components'
 
 const PostCards = () => {
+const [post, setPost]=useState([
+  {
+    id:1,
+    title:"",
+    img:"",
+    detail:"",
+    date:"",
+    author:""
+  },
+  {
+    id:1,
+    title:"",
+    img:"",
+    detail:"",
+    date:"",
+    author:""
+  },
+  {
+    id:1,
+    title:"",
+    img:"",
+    detail:"",
+    date:"",
+    author:""
+  },
+  {
+    id:1,
+    title:"",
+    img:"",
+    detail:"",
+    date:"",
+    author:""
+  },
+  {
+    id:1,
+    title:"",
+    img:"",
+    detail:"",
+    date:"",
+    author:""
+  },
+  {
+    id:1,
+    title:"",
+    img:"",
+    detail:"",
+    date:"",
+    author:""
+  },
+])
+
   return (
-    <div>PostCards</div>
+    <CardContainer>
+      <CardTitle>TITLE</CardTitle>
+      <CardWrapper>
+        <PostTitle></PostTitle>
+        <PostImg></PostImg>
+        <PostDetail></PostDetail>
+        <PostDate></PostDate>
+        <PostAuthor></PostAuthor>
+
+      </CardWrapper>
+
+    </CardContainer>
   )
 }
 
 export default PostCards
+
+const CardContainer=styled.div`
+
+`
+
+const CardTitle=styled.div`
+
+`
+
+const CardWrapper=styled.div`
+
+`
+
+const PostTitle=styled.h2`
+
+`
+
+const PostImg=styled.img`
+
+`
+
+const PostDetail=styled.h3`
+
+`
+
+const PostDate=styled.p`
+
+`
+
+const PostAuthor=styled.span`
+
+`
